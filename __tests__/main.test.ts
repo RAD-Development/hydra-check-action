156fc70700cdb884bd41b41c505f74889872d25c
ahuston-0 <aliceghuston@gmail.com>
2024-04-21 14:07:53 -0400

    fix lint errors
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -19,7 +19,7 @@ const timeRegex = /^\d{2}:\d{2}:\d{2}/
 let debugMock: jest.SpiedFunction<typeof core.debug>
 let errorMock: jest.SpiedFunction<typeof core.error>
 let getInputMock: jest.SpiedFunction<typeof core.getInput>
-let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
+// let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
 let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
 
 describe('action', () => {
@@ -29,7 +29,7 @@ describe('action', () => {
     debugMock = jest.spyOn(core, 'debug').mockImplementation()
     errorMock = jest.spyOn(core, 'error').mockImplementation()
     getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
-    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+    // setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
     setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
   })
 
@@ -75,25 +75,4 @@ describe('action', () => {
     expect(errorMock).not.toHaveBeenCalled()
   })
 
-  // it('sets a failed status', async () => {
-  //   // Set the action's inputs as return values from core.getInput()
-  //   getInputMock.mockImplementation(name => {
-  //     switch (name) {
-  //       case 'milliseconds':
-  //         return 'this is not a number'
-  //       default:
-  //         return ''
-  //     }
-  //   })
-
-  //   await main.run()
-  //   expect(runMock).toHaveReturned()
-
-  //   // Verify that all of the core library functions were called correctly
-  //   expect(setFailedMock).toHaveBeenNthCalledWith(
-  //     1,
-  //     'milliseconds not a number'
-  //   )
-  //   expect(errorMock).not.toHaveBeenCalled()
-  // })
 })
