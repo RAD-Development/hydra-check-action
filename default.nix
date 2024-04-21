d27fe9a0d99ff9b851f7a5208891a567c0f970d2
ahuston-0 <aliceghuston@gmail.com>
2024-04-21 13:23:35 -0400

    add dream2nix
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

---
+++ default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  config,
+  dream2nix,
+  ...
+}: {
+  imports = [
+    dream2nix.modules.dream2nix.nodejs-package-json-v3
+    dream2nix.modules.dream2nix.nodejs-granular-v3
+  ];
+
+  deps = {nixpkgs, ...}: {
+    inherit
+      (nixpkgs)
+      gnugrep
+      stdenv
+      ;
+  };
+
+  nodejs-granular-v3 = {
+    buildScript = ''
+      npm run format:write
+      npm run package
+      chmod +x ./dist/index.js
+      patchShebangs .
+    '';
+  };
+
+  name = lib.mkForce "typescript-action";
+  version = lib.mkForce "0.0.1";
+
+  mkDerivation = {
+    src = lib.cleanSource ./.;
+    checkPhase = ''
+      npm run format:check
+      npm run lint
+      npm run ci-test
+      npm run test:coverage -- --passWithNoTests
+    '';
+    doCheck = true;
+  };
+}
