2c372dc34aabf74e7ca7beacba5b23416b263486
ahuston-0 <aliceghuston@gmail.com>
2024-04-21 14:00:13 -0400

    fix tests
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ export async function run(): Promise<void> {
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
     core.debug(`evaluation-name: ${evaluation_name} ...`)
-    core.debug(`hydra-url ${hydra_url} ...`)
+    core.debug(`hydra-url: ${hydra_url} ...`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
