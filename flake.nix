fb52933aefb4e4a39a63a416c47e18317464ab2e
ahuston-0 <aliceghuston@gmail.com>
2024-04-25 18:58:38 -0400

    Fix global permissions issues, formatter
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- flake.nix
+++ flake.nix
@@ -14,24 +14,31 @@
       ...
     }:
     let
-      system = "x86_64-linux";
+      inherit (nixpkgs) lib;
+      systems = [ "x86_64-linux" ];
+      forEachSystem = lib.genAttrs systems;
       inherit (self) outputs;
     in
     {
+
+      formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
       # All packages defined in ./packages/<name> are automatically added to the flake outputs
       # e.g., 'packages/hello/default.nix' becomes '.#packages.hello'
-      defaultPackage.${system} = dream2nix.lib.evalModules {
-        packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
-        modules = [
-          ./default.nix
-          {
-            paths.projectRoot = ./.;
-            # can be changed to ".git" or "flake.nix" to get rid of .project-root
-            paths.projectRootFile = "flake.nix";
-            paths.package = ./.;
-          }
-        ];
-      };
+      defaultPackage = forEachSystem (
+        system:
+        dream2nix.lib.evalModules {
+          packageSets.nixpkgs = inputs.dream2nix.inputs.nixpkgs.legacyPackages.${system};
+          modules = [
+            ./default.nix
+            {
+              paths.projectRoot = ./.;
+              # can be changed to ".git" or "flake.nix" to get rid of .project-root
+              paths.projectRootFile = "flake.nix";
+              paths.package = ./.;
+            }
+          ];
+        }
+      );
 
       inherit (self) outputs;
       hydraJobs = import ./hydra/jobs.nix { inherit inputs outputs; };
